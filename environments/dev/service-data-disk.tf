e501d49eb0a1b8b7df81eb12edd391d739e03e76
SaiKumarMadugula1 <skmadugula@beon.net>
2023-10-17 15:45:30 +0200

    test

--- environments/dev/service-data-disk.tf
+++ environments/dev/service-data-disk.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project = "iac-project-397409"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
 resource "google_compute_disk" "data_disk" {
   name  = "my-application-disk"
   type  = "pd-standard"  # Choose the appropriate disk type
