b4893fd8e83b3a6a4429db50f75aeebe5bf55cda
SaiKumarMadugula1 <skmadugula@beon.net>
2023-10-18 14:37:31 +0200

    cloudbuild triggers

---
+++ environments/dev/example-cloudbuild-trigger.tf
@@ -0,0 +1,24 @@
+resource "google_cloud_build_trigger" "my_trigger" {
+  name     = "my-trigger"
+  project  = "iac-project-397409"
+  description = "Trigger for Cloud Build"
+
+  trigger_template {
+    branch_name {
+      branch_name = "dev"
+    }
+    repo_name {
+      repo_name = "SaiKumarMadugula1/solutions-terraform-cloudbuild-gitops (GitHub App)"
+    }
+  }
+
+  build {
+    name = "gcr.io/cloud-builders/gcloud"
+    args = ["builds", "submit", ".", "--config=cloudbuild.yaml"]
+    #dir  = "cloudbuild"
+  }
+
+    substitutions = {
+      _CLOUD_PUBSUB_TOPIC = "projects/iac-project-397409/topics/createschduler"
+    }
+  }
