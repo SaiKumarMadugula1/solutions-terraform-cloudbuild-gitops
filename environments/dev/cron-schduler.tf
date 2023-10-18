cf78ef7d9bc133457945ad17c517386792d796dd
SaiKumarMadugula1 <skmadugula@beon.net>
2023-10-18 09:51:17 +0200

    test create cron schduler

---
+++ environments/dev/cron-schduler.tf
@@ -0,0 +1,26 @@
+resource "google_cloud_scheduler_job" "cron_job" {
+  name     = "my-cron-job"
+  location = "us-central1"
+
+  schedule = "*/10 * * * *"  # This schedule runs the job every 10 minutes
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.my_topic.name
+    data = "Hello, Pub/Sub!"
+  }
+
+  retry_config {
+    retry_count      = 3
+    max_retry_duration = "3600s"
+  }
+}
+
+resource "google_pubsub_topic" "my_topic" {
+  name = "my-topic"
+}
+
+resource "google_pubsub_subscription" "my_subscription" {
+  name   = "my-subscription"
+  topic  = google_pubsub_topic.my_topic.name
+  ack_deadline_seconds = 10
+}
