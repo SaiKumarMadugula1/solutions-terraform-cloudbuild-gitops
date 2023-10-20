2592339f03bd600eac9ebb18db1eba6a5e53755a
SaiKumarMadugula1 <skmadugula@beon.net>
2023-10-20 15:02:09 +0200

    fix

--- environments/dev/vm-from-customimage.tf
+++ environments/dev/vm-from-customimage.tf
@@ -86,8 +86,8 @@ resource "google_compute_instance" "fedora38-java-mvn-1" {
   }
 
   service_account {
-    email  = "410804966241-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+    email  = "ansible-ssh@iac-project-397409.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/compute", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
   shielded_instance_config {
